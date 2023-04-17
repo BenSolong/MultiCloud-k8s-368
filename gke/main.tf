11985ddde10d8f16bcd2ebb77256aa3e0f372491
Jimsichu <97720745+Jimsichu@users.noreply.github.com>
2023-04-17 22:01:40 +0800

    fix bug

--- gke/main.tf
+++ gke/main.tf
@@ -61,10 +61,6 @@ resource "google_compute_subnetwork" "private2" {
   }
 }
 
-resource "google_sql_database_instance" "name" {
-  
-}
-
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_router
 resource "google_compute_router" "router" {
   name    = "router"
